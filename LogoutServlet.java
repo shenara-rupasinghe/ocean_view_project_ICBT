863f088c770719bb8b3f152e438ed96da9588b19
shenara-rupasinghe <rjmshenarathimashirupasinghe@gmail.com>
2026-03-06 17:00:00 +0530

    Integrated secure logout mechanism and session invalidation logic

---
+++ LogoutServlet.java
@@ -0,0 +1,22 @@
+package com.oceanview.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+
+        response.sendRedirect("login.jsp?msg=LoggedOut");
+    }
+}
